0c3c9fb6794c6f44491359a84c78d95c4f60e2a6
Sharifah Asilah Syed Omar <39157162+asilaaah@users.noreply.github.com>
2020-12-16 23:51:57 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,12 +5,19 @@ $gender = $_POST['gender'];
 $age = $_POST['age'];
 $email = $_POST['email'];
 if (!empty($username) || !empty($password) || !empty($gender) || !empty($age) || !empty($email)) {
- $host = "localhost";
-    $dbUsername = "root";
-	$dbPassword = "";
-    $dbname = "mysql";
-    //create connection
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
     if (mysqli_connect_error()) {
      die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
     } else {
